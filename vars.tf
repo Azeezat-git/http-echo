c138cabc86950803aa0ef32ca26de2029813f839
Azeezat-git <bimsol4real@gmail.com>
2024-01-14 15:29:26 +0100

    new-commit

--- vars.tf
+++
@@ -1,9 +0,0 @@
-variable "az_count" {
-    description = "Number of AZs to cover in a given region"
-    default = "2"
-}
-
-variable "app_image" {
-    description = "Docker image to run in the ECS cluster"
-    default = "hashicorp/http-echo"
-}
